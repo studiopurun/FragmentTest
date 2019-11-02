713efc157086be330993b06c5bec088f1206312f
studiopurun <studiopurun@gmail.com>
2019-11-02 23:28:11 +0900

    Default

--- app/src/main/java/com/example/fragmenttest/FragmentB.java
+++ app/src/main/java/com/example/fragmenttest/FragmentB.java
@@ -11,19 +11,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentA extends Fragment {
+public class FragmentB extends Fragment {
 
     Button btn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.frament_a, container, false);
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.frament_b, container, false);
         btn = (Button)view.findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "This is Franment A", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "This is Fragment B", Toast.LENGTH_SHORT).show();
             }
         });
         return view;
--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isFragmentA = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,33 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft= fm.beginTransaction();
         ft.add(R.id.frameLayout, new FragmentA());
+        ft.commit();
+
+        Button switchBtn = (Button)findViewById(R.id.switchBtn);
+        switchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchFragment();
+            }
+        });
+
+
+    }
+
+    public void switchFragment(){
+        Fragment fr;
+
+        if(isFragmentA){
+            fr= new FragmentB();
+            isFragmentA = false;
+        } else{
+            fr = new FragmentA();
+            isFragmentA = true;
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft= fm.beginTransaction();
+        ft.add(R.id.frameLayout, fr);
         ft.commit();
     }
 }
